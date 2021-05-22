3bda074129e8640e30c536f4aa5f377d8888d185
Guille <ggalmazor@gmail.com>
2021-05-22 09:22:49 +0200

    Setup: read the items JSON file path from the first argument of main.js
    Also add instructions in case no argument is passed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import fs from 'fs';
 import {update} from './update.js';
 
-update('./items.json');
+const itemsPath = process.argv[2];
+if (itemsPath === undefined) {
+  console.error("The first argument must include a path to the items JSON file");
+  console.log();
+  console.log("Usage (using `npm`):");
+  console.log("\t$ npm run update -- src/items.json");
+  console.log();
+  console.log("Usage (using `node`):");
+  console.log("\t$ node --experimental-vm-modules src/main.js src/items.json");
+  process.exit(1);
+}
 
-JSON.parse(fs.readFileSync('./items.json').toString())
+update(itemsPath);
+
+JSON.parse(fs.readFileSync(itemsPath).toString())
   .forEach(({name, sellIn, quality}) => console.log(`${name} [${sellIn} ${quality}]`));
